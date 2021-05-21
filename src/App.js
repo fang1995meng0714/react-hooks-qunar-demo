4333282e9ee97e1ccc5111de83d2526cc99a9c1f
fangyuemeng <yuemeng.fang@macrowing.com>
2021-05-21 16:25:15 +0800

    新增静态页面

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from './header/Header';
+import Journey from './journey/Journey';
 
 function App() {
+
+  const onBack=()=> {
+    console.log(23423)
+  }
   return (
     <div>
-        hello world
+      <div className="header-wrapper">
+        <Header title="火车票" onBack={onBack}></Header>
+      </div>
+      <form className="form" action="">
+        <Journey></Journey>
+      </form>
     </div>
   )
 }
---
+++ src/journey/Journey.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import switchImg from '../imgs/switch.svg';
+import './Journey.css';
+
+export default function Journey(props) {
+    return(
+        <div className="journey">
+            <div className="journey-station">
+                <input type="text" 
+                    readOnly name="from" 
+                    value={13} 
+                    className="journey-input journey-from"
+                />
+            </div>
+            <div className="journey-switch">
+                <img src={switchImg} width="70" height="40" alt="switch" />
+            </div>
+            <div className="journey-station">
+                <input type="text" 
+                    readOnly name="to" 
+                    value={13} 
+                    className="journey-input journey-to"
+                />
+            </div>
+        </div>
+    )
+}
