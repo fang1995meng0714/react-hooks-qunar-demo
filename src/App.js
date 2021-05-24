d333acc6cf640c8054735f9d7d33c0cf1b214f1b
fangyuemeng <yuemeng.fang@macrowing.com>
2021-05-24 13:25:35 +0800

    增加mock数据接口

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import DepartDate from './depart-date/DepartDate';
 import Header from './header/Header';
 import HighSpeed from './high-speed/HighSpeed';
 import Journey from './journey/Journey';
 import Submit from './submit/Submit';
+import axios from 'axios'
+import "./mock/mocker";
 
 function App() {
+  useEffect(() => {
+  	//get请求
+    axios.get('/rest/cities').then(res => {
+        console.log(res.data)
+    })
+  }, [])
 
   const onBack=()=> {
     console.log(23423)
